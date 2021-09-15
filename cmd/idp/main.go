be59e8f1e74abfbc02102aad547a9521b23cb030
Victor Ragojos <vhsvragojos@gmail.com>
2021-09-15 01:18:17 -0700

    Update main
    - Replace godotenv with new config internal package
    - Add verification
    - Delete redis store from session manager
    - Add SecurityMiddleware into router

--- cmd/idp/main.go
+++ cmd/idp/main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"log"
-	"os"
-	"time"
 
+	"github.com/RagOfJoes/idp/email"
 	loginGorm "github.com/RagOfJoes/idp/flow/login/repository/gorm"
 	loginService "github.com/RagOfJoes/idp/flow/login/service"
 	loginTransport "github.com/RagOfJoes/idp/flow/login/transport"
 	registrationGorm "github.com/RagOfJoes/idp/flow/registration/repository/gorm"
 	registrationService "github.com/RagOfJoes/idp/flow/registration/service"
 	registrationTransport "github.com/RagOfJoes/idp/flow/registration/transport"
+	verificationGorm "github.com/RagOfJoes/idp/flow/verification/repository/gorm"
+	verificationService "github.com/RagOfJoes/idp/flow/verification/service"
+	verificationTransport "github.com/RagOfJoes/idp/flow/verification/transport"
+	"github.com/RagOfJoes/idp/internal/config"
 	"github.com/RagOfJoes/idp/persistence"
 	"github.com/RagOfJoes/idp/session"
 	"github.com/RagOfJoes/idp/transport"
@@ -21,93 +24,74 @@ import (
 	identityGorm "github.com/RagOfJoes/idp/user/identity/repository/gorm"
 	identityService "github.com/RagOfJoes/idp/user/identity/service"
 	identityTransport "github.com/RagOfJoes/idp/user/identity/transport"
-	"github.com/alexedwards/scs/redisstore"
-	_ "github.com/go-playground/validator/v10"
-	"github.com/gomodule/redigo/redis"
-	"github.com/joho/godotenv"
 )
 
 func init() {
-	// Load environment variables
-	err := godotenv.Load()
-	if err != nil {
+	// Load configuration
+	if err := config.Setup("config", "yaml", "."); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func main() {
-	db, err := persistence.NewGorm(persistence.GormConfig{
-		DSN:     os.Getenv("ORM_DSN"),
-		Migrate: os.Getenv("ORM_AUTOMIGRATE") == "true",
-	})
+	cfg := config.Get()
+
+	db, err := persistence.NewGorm()
 	if err != nil {
 		log.Panic(err.Error())
 		return
 	}
 
+	// Setup Email client
+	email := email.New()
+
 	// Setup repositories
 	cor := contactGorm.NewGormContactRepository(db)
 	cr := credentialGorm.NewGormCredentialRepository(db)
 	ir := identityGorm.NewGormUserRepository(db)
+	vr := verificationGorm.NewGormVerificationRepository(db)
 	rr := registrationGorm.NewGormRegistrationRepository(db)
 	lr := loginGorm.NewGormLoginRepository(db)
 	// Setup services
 	cos := contactService.NewContactService(cor)
-	ap := credentialService.NewArgonParams(64*1024, 2, 2, 16, 32)
-	cs := credentialService.NewCredentialService(ap, cr)
+	cs := credentialService.NewCredentialService(cr)
 	is := identityService.NewIdentityService(ir)
 	// Flow Services
 	// These will essentially stitch all other services together
+	vs := verificationService.NewVerificationService(email, vr, cos, cs, is)
 	rs := registrationService.NewRegistrationService(rr, cos, cs, is)
 	ls := loginService.NewLoginService(lr, cos, cs, is)
 
-	// Setup HTTP transport
 	// Create session manager
-	pool := &redis.Pool{
-		MaxIdle: 10,
-		Dial: func() (redis.Conn, error) {
-			conn, err := redis.Dial("tcp", os.Getenv("REDIS_URL"))
-			if err != nil {
-				return nil, err
-			}
-			if _, err := conn.Do("AUTH", os.Getenv("REDIS_PASSWORD")); err != nil {
-				return nil, err
-			}
-			return conn, nil
-		},
-	}
-	sessionManager, err := session.NewManager(false, "sid", time.Hour*24*14)
-	sessionManager.Store = redisstore.New(pool)
+	sessionManager, err := session.NewManager()
 	if err != nil {
 		log.Panic(err)
 	}
 
-	// Setup HTTP config
-	httpSrvCfg := transport.HttpConfig{
-		Host:   os.Getenv("HOST"),
-		Port:   os.Getenv("PORT"),
-		Scheme: os.Getenv("SCHEME"),
-
-		RemoveExtraSlashes: true,
-	}
 	// Setup HTTP Server
-	ginEng := transport.NewHttp(httpSrvCfg)
+	router := transport.NewHttp()
+
 	// Attach Middlewares
 	//
 	// Order of execution:
 	// 1. Rate Limiter
-	// 2. Auth Middleware (Checks session for identity if found then passes to context)
-	// 3. Execute route
-	// 4. Error Middleware handles any errors that were generated from route execution
-	ginEng.Use(transport.RateLimiterMiddleware(100), session.AuthMiddleware(sessionManager), transport.ErrorMiddleware())
+	// 2. Security Middleware (Adds essential security headers to request)
+	// 3. Auth Middleware (Checks session for identity if found then passes to context)
+	// 4. Execute route
+	// 5. Error Middleware handles any errors that were generated from route execution
+	if cfg.Server.RPS > 0 {
+		router.Use(transport.RateLimiterMiddleware(cfg.Server.RPS))
+	}
+	router.Use(transport.SecurityMiddleware(), session.AuthMiddleware(sessionManager, is), transport.ErrorMiddleware())
 
 	// Attach routes
-	identityTransport.NewIdentityHttp(sessionManager, ginEng)
-	registrationTransport.NewRegistrationHttp(rs, sessionManager, ginEng)
-	loginTransport.NewLoginHttp(ls, sessionManager, ginEng)
+	identityTransport.NewIdentityHttp(sessionManager, router)
+	verificationTransport.NewVerificationHttp(sessionManager, vs, router)
+	registrationTransport.NewRegistrationHttp(rs, vs, sessionManager, router)
+	loginTransport.NewLoginHttp(ls, sessionManager, router)
 
 	// Start HTTP server
-	if err := transport.RunHttp(httpSrvCfg, sessionManager.LoadAndSave(ginEng)); err != nil {
+	if err := transport.RunHttp(sessionManager.LoadAndSave(router)); err != nil {
 		log.Panic(err)
 	}
 }
