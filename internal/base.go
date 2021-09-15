454eefb8e8acb2b03a9ed88553c5a80f870b5ba2
Victor Ragojos <vhsvragojos@gmail.com>
2021-09-14 23:40:23 -0700

    Add internal folder
    - Move base.go into internal folder
    - Move error.go into internal folder
    - Delete common.go

--- internal/base.go
+++ internal/base.go
@@ -1,4 +1,4 @@
-package idp
+package internal
 
 import (
 	"time"
@@ -7,12 +7,14 @@ import (
 	"gorm.io/gorm"
 )
 
+// Base defines the base model for domain objects
 type Base struct {
 	ID        uuid.UUID  `json:"id" gorm:"primaryKey;type:uuid;default:uuid_generate_v4()" validate:"required,uuid4"`
 	CreatedAt time.Time  `json:"created_at" gorm:"index;not null;default:current_timestamp" validate:"required"`
 	UpdatedAt *time.Time `json:"updated_at,omitempty" gorm:"index;default:null"`
 }
 
+// BaseSoftDelete defines the base model with soft delete functionality for domain objects
 type BaseSoftDelete struct {
 	ID        uuid.UUID      `json:"id" gorm:"primaryKey;type:uuid;default:uuid_generate_v4()" validate:"required"`
 	CreatedAt time.Time      `json:"created_at" gorm:"index;not null;default:current_timestamp" validate:"required"`
--- internal/error.go
+++ internal/error.go
@@ -1,4 +1,4 @@
-package idp
+package internal
 
 import (
 	"fmt"
