be324df37c00ff449d9abf7399ca83f65d84f70b
Victor Ragojos <vhsvragojos@gmail.com>
2021-09-28 00:30:33 -0700

    Update base.go
    Remove uuid4 field in validate struct tag since it doesn't seem to work properly with repositories

--- internal/base.go
+++ internal/base.go
@@ -9,7 +9,7 @@ import (
 
 // Base defines the base model for domain objects
 type Base struct {
-	ID        uuid.UUID  `json:"id" gorm:"primaryKey;type:uuid;default:uuid_generate_v4()" validate:"required,uuid4"`
+	ID        uuid.UUID  `json:"id" gorm:"primaryKey;type:uuid;default:uuid_generate_v4()" validate:"required"`
 	CreatedAt time.Time  `json:"created_at" gorm:"index;not null;default:current_timestamp" validate:"required"`
 	UpdatedAt *time.Time `json:"updated_at,omitempty" gorm:"index;default:null"`
 }
