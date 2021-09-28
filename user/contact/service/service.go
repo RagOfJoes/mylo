6eb2fb157a1205a5c3e3578c5586decef82dce57
Victor Ragojos <vhsvragojos@gmail.com>
2021-09-28 00:31:25 -0700

    Update contact's service
    Update errors to new structure, simplify summary/description, and utilize details

--- user/contact/service/service.go
+++ user/contact/service/service.go
@@ -10,8 +10,10 @@ import (
 )
 
 var (
-	errAddInvalidContacts = func(err error) error {
-		return internal.NewServiceClientError(err, "contact_add", "Invalid contact values provided", nil)
+	errAddInvalidContacts = func(src error, i uuid.UUID) error {
+		return internal.NewServiceClientError(src, "Contact_FailedAdd", "Invalid contact values provided", map[string]interface{}{
+			"IdentityID": i,
+		})
 	}
 )
 
@@ -27,18 +29,20 @@ func NewContactService(cr contact.Repository) contact.Service {
 
 func (s *service) Add(args ...contact.Contact) ([]contact.Contact, error) {
 	if len(args) == 0 {
-		return nil, errAddInvalidContacts(nil)
+		return nil, errAddInvalidContacts(nil, uuid.UUID{})
 	}
 
 	identityID := args[0].IdentityID
 	if err := s.cr.DeleteAllUser(identityID); err != nil {
 		_, file, line, _ := runtime.Caller(1)
-		return nil, internal.NewServiceInternalError(file, line, "contact_add", fmt.Sprintf("Failed to delete %s contacts", identityID))
+		return nil, internal.NewServiceInternalError(err, file, line, "Contact_FailedDelete", fmt.Sprintf("Failed to delete %s contacts", identityID), map[string]interface{}{
+			"IdentityID": identityID,
+		})
 	}
 
 	n, err := s.cr.Create(args...)
 	if err != nil {
-		return nil, errAddInvalidContacts(err)
+		return nil, errAddInvalidContacts(err, identityID)
 	}
 	return n, nil
 }
@@ -48,7 +52,7 @@ func (s *service) Find(i string) (*contact.Contact, error) {
 	if err == nil {
 		f, err := s.cr.Get(uid)
 		if err != nil {
-			return nil, internal.NewServiceClientError(err, "contact_find", "Invalid ID provided", &map[string]interface{}{
+			return nil, internal.NewServiceClientError(err, "Contact_FailedFind", "Contact does not exist", map[string]interface{}{
 				"ContactID": uid,
 			})
 		}
@@ -56,7 +60,7 @@ func (s *service) Find(i string) (*contact.Contact, error) {
 	}
 	f, err := s.cr.GetByValue(i)
 	if err != nil {
-		return nil, internal.NewServiceClientError(err, "contact_find", "Invalid contact value provided", &map[string]interface{}{
+		return nil, internal.NewServiceClientError(err, "Contact_FailedFind", "Contact does not exist", map[string]interface{}{
 			"Contact": contact.Contact{Value: i},
 		})
 	}
