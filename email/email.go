bc94ad8bb30a04f4c9275c4cad9f7a717565e9cf
Victor Ragojos <vhsvragojos@gmail.com>
2021-09-27 23:59:14 -0700

    Update email.go
    Update errors to new structure, simplify summary/description, and utilize details

--- email/email.go
+++ email/email.go
@@ -12,9 +12,11 @@ import (
 )
 
 var (
-	errInvalidTemplate = func(t string) error {
+	errInvalidTemplate = func(src error, template, to string) error {
 		_, file, line, _ := runtime.Caller(1)
-		return internal.NewServiceInternalError(file, line, "email_invalid_template", fmt.Sprintf("Invalid %s template data provided", t))
+		return internal.NewServiceInternalError(src, file, line, "Email_InvalidTemplate", fmt.Sprintf("Invalid %s template data provided", template), map[string]interface{}{
+			"Email": to,
+		})
 	}
 )
 
@@ -46,7 +48,9 @@ func New() Client {
 func (c *client) Send(to string, template Template, data interface{}) error {
 	if err := validate.Var(to, "email"); err != nil {
 		_, file, line, _ := runtime.Caller(1)
-		return internal.NewServiceInternalError(file, line, "email_invalid_email", fmt.Sprintf("Value, %s, provided for the argument `to` must be a valid email.", to))
+		return internal.NewServiceInternalError(err, file, line, "", fmt.Sprintf("Value, %s, provided for the argument `to` must be a valid email.", to), map[string]interface{}{
+			"Email": to,
+		})
 	}
 
 	payload := Payload{}
@@ -55,7 +59,7 @@ func (c *client) Send(to string, template Template, data interface{}) error {
 	case Welcome:
 		welcomeData, ok := data.(WelcomeTemplateData)
 		if !ok {
-			return errInvalidTemplate("Welcome")
+			return errInvalidTemplate(nil, "Welcome", to)
 		}
 
 		mapData, err := structToMap(welcomeData)
@@ -67,7 +71,7 @@ func (c *client) Send(to string, template Template, data interface{}) error {
 	case Verification:
 		verificationData, ok := data.(VerificationTemplateData)
 		if !ok {
-			return errInvalidTemplate("Verification")
+			return errInvalidTemplate(nil, "Verification", to)
 		}
 		mapData, err := structToMap(verificationData)
 		if err != nil {
@@ -76,7 +80,7 @@ func (c *client) Send(to string, template Template, data interface{}) error {
 		payload.TemplateID = c.welcomeID
 		personalization.DynamicTemplateData = mapData
 	default:
-		return errInvalidTemplate(string(template))
+		return errInvalidTemplate(nil, string(template), to)
 	}
 	payload.From = c.sender
 	personalization.To = append(personalization.To, &Email{Email: to})
@@ -85,7 +89,10 @@ func (c *client) Send(to string, template Template, data interface{}) error {
 	body, err := json.Marshal(payload)
 	if err != nil {
 		_, file, line, _ := runtime.Caller(1)
-		return internal.NewServiceInternalError(file, line, "email_failed_marshal", "Failed to marshal payload")
+		return internal.NewServiceInternalError(err, file, line, "Email_FailedMarshal", "Failed to marshal payload", map[string]interface{}{
+			"Email":   to,
+			"Payload": payload,
+		})
 	}
 
 	request := sendgrid.GetRequest(c.apiKey, "/v3/mail/send", c.host)
@@ -94,7 +101,10 @@ func (c *client) Send(to string, template Template, data interface{}) error {
 
 	if _, err := sendgrid.API(request); err != nil {
 		_, file, line, _ := runtime.Caller(1)
-		return internal.NewServiceInternalError(file, line, "email_failed_send", err.Error())
+		return internal.NewServiceInternalError(err, file, line, "Email_FailedSend", err.Error(), map[string]interface{}{
+			"Email":   to,
+			"Payload": payload,
+		})
 	}
 	return nil
 }
@@ -105,11 +115,11 @@ func structToMap(d interface{}) (map[string]interface{}, error) {
 	bytes, err := json.Marshal(d)
 	if err != nil {
 		_, file, line, _ := runtime.Caller(1)
-		return nil, internal.NewServiceInternalError(file, line, "email_failed_marshal", "Failed to marshal template data")
+		return nil, internal.NewServiceInternalError(err, file, line, "Email_FailedMarshal", "Failed to marshal template data", nil)
 	}
 	if err := json.Unmarshal(bytes, &m); err != nil {
 		_, file, line, _ := runtime.Caller(1)
-		return nil, internal.NewServiceInternalError(file, line, "email_failed_unmarshal", "Failed to unmarshal template data")
+		return nil, internal.NewServiceInternalError(err, file, line, "Email_FailedUnmarshal", "Failed to unmarshal template data", nil)
 	}
 	return m, nil
 }
