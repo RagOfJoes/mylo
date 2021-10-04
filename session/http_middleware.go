8f8dee3275f22333ac3d51bed861f5b097d23ad2
Victor Ragojos <vhsvragojos@gmail.com>
2021-10-03 19:17:03 -0700

    Update session to destroy if identity no longer exists

--- session/http_middleware.go
+++ session/http_middleware.go
@@ -9,16 +9,24 @@ import (
 // and passes it to context
 func AuthMiddleware(sm *Manager, is identity.Service) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		sess := sm.Retrieve(c.Request.Context(), true)
+		ctx := c.Request.Context()
+		sess := sm.Retrieve(ctx, true)
 		if sess != nil {
 			u, err := is.Find(sess.IdentityID.String())
-			if u != nil && err == nil {
+			// If user does not exist anymore
+			// - Update session retrieved to nil
+			// - Remove session from manager
+			// - Update context of request
+			if err != nil || u == nil {
+				sess = nil
+				sm.Remove(ctx, "sess")
+				c.Request = c.Request.WithContext(ctx)
+			} else if u != nil && err == nil {
 				sess.Identity = u
 				sess.Contacts = u.Contacts
-				c.Set("sess", sess)
 			}
 		}
-
+		c.Set("sess", sess)
 		c.Next()
 	}
 }
