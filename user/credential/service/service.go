d1a4b2ba089b01addb4d599c3e07095f58e7907f
Victor Ragojos <vhsvragojos@gmail.com>
2021-10-19 14:02:45 -0700

    Update credential service
    - Add errors
    - Update docs
    - Add FindPasswordWithIdentifier method
    - Add UpdatePassword method

--- user/credential/service/service.go
+++ user/credential/service/service.go
@@ -17,6 +17,39 @@ var (
 			"IdentityID": i,
 		})
 	}
+	errWeakPassword = func(src error, i uuid.UUID, identifiers []credential.Identifier) error {
+		return internal.NewServiceClientError(nil, "Credential_WeakPassword", "Password provided is too weak", map[string]interface{}{
+			"IdentityID":  i,
+			"Identifiers": identifiers,
+		})
+	}
+	errFailedGeneratePassword = func(src error, i uuid.UUID, identifiers []credential.Identifier) error {
+		_, file, line, _ := runtime.Caller(1)
+		return internal.NewServiceInternalError(src, file, line, "Credential_FailedPassword", "Failed to generate a hashed password", map[string]interface{}{
+			"IdentityID":  i,
+			"Identifiers": identifiers,
+		})
+	}
+	errFailedCompare = func(src error, i uuid.UUID) error {
+		_, file, line, _ := runtime.Caller(1)
+		return internal.NewServiceInternalError(src, file, line, "Credential_FailedPassword", "Failed to compare password and hash", map[string]interface{}{
+			"IdentityID": i,
+		})
+	}
+	errFailedDecodePassword = func(src error, i uuid.UUID) error {
+		_, file, line, _ := runtime.Caller(1)
+		return internal.NewServiceInternalError(src, file, line, "Credential_FailedPassword", "Failed to decode password credentials", map[string]interface{}{
+			"IdentityID": i,
+		})
+	}
+	errFailedEncodePassword = func(src error, i uuid.UUID, ids []credential.Identifier) error {
+		_, file, line, _ := runtime.Caller(1)
+		return internal.NewServiceInternalError(src, file, line, "Credential_FailedPassword", "Failed to JSON encode hashed password", map[string]interface{}{
+			"IdentityID":  i,
+			"Identifiers": ids,
+		})
+
+	}
 )
 
 type service struct {
@@ -39,39 +72,28 @@ func (s *service) CreatePassword(uid uuid.UUID, password string, identifiers []c
 	// Test password strength
 	passStrength := zxcvbn.PasswordStrength(password, ids)
 	if passStrength.Score <= cfg.Credential.MinimumScore {
-		return nil, internal.NewServiceClientError(nil, "Credential_WeakPassword", "Password provided is too weak", map[string]interface{}{
-			"IdentityID":  uid,
-			"Identifiers": identifiers,
-		})
+		return nil, errWeakPassword(nil, uid, identifiers)
 	}
-	// 3. Hash password
+	// Hash password
 	newPass, err := generateFromPassword(password)
 	if err != nil {
-		_, file, line, _ := runtime.Caller(1)
-		return nil, internal.NewServiceInternalError(err, file, line, "Credential_FailedPassword", "Failed to generate a hashed password", map[string]interface{}{
-			"IdentityID":  uid,
-			"Identifiers": identifiers,
-		})
+		return nil, errFailedGeneratePassword(err, uid, identifiers)
 	}
 	credPass := credential.CredentialPassword{
 		HashedPassword: newPass,
 	}
 	jsonPass, err := json.Marshal(credPass)
 	if err != nil {
-		_, file, line, _ := runtime.Caller(1)
-		return nil, internal.NewServiceInternalError(err, file, line, "Credential_FailedPassword", "Failed to JSON encode hashed password", map[string]interface{}{
-			"IdentityID":  uid,
-			"Identifiers": identifiers,
-		})
+		return nil, errFailedEncodePassword(err, uid, identifiers)
 	}
-	// 4. Build Credential
+	// Build Credential
 	newCredential := credential.Credential{
 		Type:        credential.Password,
 		IdentityID:  uid,
 		Identifiers: identifiers,
 		Values:      string(jsonPass[:]),
 	}
-	// 5. Create Credential in repository
+	// Create Credential in repository
 	ncp, err := s.cr.Create(newCredential)
 	if err != nil {
 		return nil, internal.NewServiceClientError(err, "Credential_FailedCreate", "Invalid identifier(s)/password provided", map[string]interface{}{
@@ -89,20 +111,85 @@ func (s *service) ComparePassword(uid uuid.UUID, password string) error {
 	}
 	var hashed credential.CredentialPassword
 	if err := json.Unmarshal([]byte(cred.Values), &hashed); err != nil {
-		_, file, line, _ := runtime.Caller(1)
-		return internal.NewServiceInternalError(err, file, line, "Credential_FailedPassword", "Failed to decode password credentials", map[string]interface{}{
-			"IdentityID": uid,
-		})
+		return errFailedDecodePassword(err, uid)
 	}
 	match, err := comparePasswordAndHash(password, hashed.HashedPassword)
 	if err != nil {
-		_, file, line, _ := runtime.Caller(1)
-		return internal.NewServiceInternalError(err, file, line, "Credential_FailedPassword", "Failed to compare password and hash", map[string]interface{}{
-			"IdentityID": uid,
-		})
+		return errFailedCompare(err, uid)
 	}
 	if !match {
 		return errFailedFind(err, uid)
 	}
 	return nil
+}
+
+func (s *service) FindPasswordWithIdentifier(identifier string) (*credential.Credential, error) {
+	credential, err := s.cr.GetWithIdentifier(credential.Password, identifier)
+	if err != nil {
+		return nil, internal.NewServiceClientError(err, "Credential_FailedFind", "Invalid identifier provided", map[string]interface{}{
+			"Identifier": identifier,
+		})
+	}
+	return credential, nil
+}
+
+func (s *service) UpdatePassword(uid uuid.UUID, newPassword string) (*credential.Credential, error) {
+	// Find existing credential
+	cred, err := s.cr.GetWithIdentityID(credential.Password, uid)
+	if err != nil {
+		return nil, internal.NewServiceClientError(err, "Credential_FailedFind", "The account doesn't exist or the account doesn't have a password credential setup", map[string]interface{}{
+			"IdentityID": uid,
+		})
+	}
+	cfg := config.Get()
+	// Get identifiers to test password strength
+	ids := []string{}
+	for _, id := range cred.Identifiers {
+		ids = append(ids, id.Value)
+	}
+	// Test password strength
+	passStrength := zxcvbn.PasswordStrength(newPassword, ids)
+	if passStrength.Score <= cfg.Credential.MinimumScore {
+		return nil, errWeakPassword(nil, uid, cred.Identifiers)
+	}
+	// Compare new and old password
+	var hashed credential.CredentialPassword
+	if err := json.Unmarshal([]byte(cred.Values), &hashed); err != nil {
+		return nil, errFailedDecodePassword(err, uid)
+	}
+	match, err := comparePasswordAndHash(newPassword, hashed.HashedPassword)
+	if err != nil {
+		return nil, errFailedCompare(err, uid)
+	}
+	if match {
+		return nil, internal.NewServiceClientError(nil, "Credential_FailedUpdate", "Invalid password provided", map[string]interface{}{
+			"IdentityID":  uid,
+			"NewPassword": newPassword,
+		})
+	}
+	// Create new password
+	newPass, err := generateFromPassword(newPassword)
+	if err != nil {
+		return nil, errFailedGeneratePassword(err, uid, cred.Identifiers)
+	}
+	credPass := credential.CredentialPassword{
+		HashedPassword: newPass,
+	}
+	jsonPass, err := json.Marshal(credPass)
+	if err != nil {
+		return nil, errFailedEncodePassword(err, uid, cred.Identifiers)
+	}
+	// Build Credential
+	uc := *cred
+	uc.Values = string(jsonPass[:])
+	// Update password
+	updated, err := s.cr.Update(uc)
+	if err != nil {
+		_, file, line, _ := runtime.Caller(1)
+		return nil, internal.NewServiceInternalError(err, file, line, "Credential_FailedUpdate", "Failed to update credential", map[string]interface{}{
+			"IdentityID":        uid,
+			"UpdatedCredential": uc,
+		})
+	}
+	return updated, nil
 }
