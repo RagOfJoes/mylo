3cbf9c21de880d38ee4f0a31f74da9deb26d0862
Victor Ragojos <vhsvragojos@gmail.com>
2021-11-04 12:21:09 -0700

    Remove implementation of old session

--- transport/http_util.go
+++ transport/http_util.go
@@ -3,27 +3,7 @@ package transport
 import (
 	"fmt"
 	"net/http"
-	"time"
-
-	"github.com/RagOfJoes/idp/session"
-	"github.com/gin-gonic/gin"
 )
-
-// IsAuthenticated checks context for identity
-func IsAuthenticated(ctx *gin.Context) *session.Session {
-	v, ok := ctx.Get("sess")
-	if !ok || v == nil {
-		return nil
-	}
-	session, ok := v.(*session.Session)
-	if !ok || session == nil {
-		return nil
-	}
-	if session.ExpiresAt.Before(time.Now()) || session.Identity == nil {
-		return nil
-	}
-	return session
-}
 
 // RequestURL retrieves entry path of request
 func RequestURL(req *http.Request) string {
