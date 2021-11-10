9e74e19cb117eaf99c27657ebcd0b931f7bbc2a7
Victor Ragojos <vhsvragojos@gmail.com>
2021-11-10 13:18:35 -0800

    Update session/transport/http
    - Add docs
    - Add SessionOrNew and SessionOrNewAndSetCookie to consolidate fn calls
    - Add mustBeAuthenticated param in Session method

--- session/transport/http.go
+++ session/transport/http.go
@@ -35,6 +35,7 @@ func (h *Http) New(req *http.Request, w http.ResponseWriter) (*session.Session, 
 	return created, nil
 }
 
+// Upsert will update a session if it exists and if not then it will create a new one
 func (h *Http) Upsert(s session.Session) (*session.Session, error) {
 	_, err := h.se.FindByID(s.ID)
 	if err != nil {
@@ -68,6 +69,7 @@ func (h *Http) SetCookie(req *http.Request, w http.ResponseWriter, s session.Ses
 	return nil
 }
 
+// UpsertAndSetCookie will call Upsert method then SetCookie method
 func (h *Http) UpsertAndSetCookie(req *http.Request, w http.ResponseWriter, s session.Session) (*session.Session, error) {
 	upserted, err := h.Upsert(s)
 	if err != nil {
@@ -79,8 +81,10 @@ func (h *Http) UpsertAndSetCookie(req *http.Request, w http.ResponseWriter, s se
 	return upserted, nil
 }
 
-// Session retrieves session token from Request then fetches from service
-func (h *Http) Session(req *http.Request, w http.ResponseWriter) (*session.Session, error) {
+// Session retrieves session token from Request then fetches from service.
+//
+// If mustBeAuthenticated is true then will return error if session is not authenticated
+func (h *Http) Session(req *http.Request, w http.ResponseWriter, mustBeAuthenticated bool) (*session.Session, error) {
 	token := h.getToken(req)
 	if token == "" {
 		return nil, transport.NewHttpClientError(nil, http.StatusUnauthorized, "Session_NotFound", "No active session", nil)
@@ -92,6 +96,45 @@ func (h *Http) Session(req *http.Request, w http.ResponseWriter) (*session.Sessi
 	}
 	if found.IdentityID != nil && found.Identity != nil {
 		found.Identity.Credentials = nil
+	}
+	return found, nil
+}
+
+// SessionOrNew will retrieve a session if it exists and if not then create a new one. If a new session needs to be created then mustBeAuthenticated is ignored
+func (h *Http) SessionOrNew(req *http.Request, w http.ResponseWriter, mustBeAuthenticated bool) (*session.Session, error) {
+	token := h.getToken(req)
+	if token == "" {
+		return nil, internal.NewErrorf(internal.ErrorCodeUnauthorized, "%v", session.ErrSessionNotFound)
+	}
+	found, _ := h.Session(req, w, false)
+	if found == nil {
+		newSession, err := h.New(req, w)
+		if err != nil {
+			return nil, err
+		}
+		return newSession, nil
+	}
+	if mustBeAuthenticated && found != nil && !found.Authenticated() {
+		return nil, internal.NewErrorf(internal.ErrorCodeUnauthorized, "%v", session.ErrSessionNotFound)
+	}
+	return found, nil
+}
+
+// SessionOrNewAndSetCookie does exactly what SessionOrNew does except it sets a session cookie when a new session needs to be created
+func (h *Http) SessionOrNewAndSetCookie(req *http.Request, w http.ResponseWriter, mustBeAuthenticated bool) (*session.Session, error) {
+	found, _ := h.Session(req, w, false)
+	if found == nil {
+		newSession, err := h.New(req, w)
+		if err != nil {
+			return nil, err
+		}
+		if err := h.SetCookie(req, w, *newSession); err != nil {
+			return nil, err
+		}
+		return newSession, nil
+	}
+	if mustBeAuthenticated && found != nil && !found.Authenticated() {
+		return nil, internal.NewErrorf(internal.ErrorCodeUnauthorized, "%v", session.ErrSessionNotFound)
 	}
 	return found, nil
 }
