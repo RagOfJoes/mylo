981448d15365cde202411614b92bc197e148c499
RagOfJoes <apps@ragofjoes.dev>
2021-11-23 19:51:01 +0000

    Update EOL sequence to LF

--- docker/initdb.sh
+++ docker/initdb.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB"  <<-EOSQL
-  CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB" <<-EOSQL
+ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 EOSQL
